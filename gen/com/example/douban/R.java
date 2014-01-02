368fa6f50318fb5fadfe645b191db420c9ab9f2e
dongchunmei <997782609@qq.com>
2014-01-02 13:33:35 -0800

    登录授权及新增评论
    1、添加评论界面；
    2、用户授权登录界面；
    3、用户登录授权功能实现；
    4、添加评论功能实现；
    5、图书评论列表排序规则（按热门、按时间）切换功能实现；

--- gen/com/example/douban/R.java
+++ gen/com/example/douban/R.java
@@ -44,43 +44,53 @@ public final class R {
         public static final int tab_main_nav_search=0x7f02000c;
         public static final int title_bg_night=0x7f02000d;
         public static final int wp_button=0x7f02000e;
+        public static final int wp_button_small=0x7f02000f;
+        public static final int wp_small=0x7f020010;
+        public static final int wp_small_down=0x7f020011;
+        public static final int wp_small_over=0x7f020012;
     }
     public static final class id {
-        public static final int author_name=0x7f08001a;
-        public static final int back_button=0x7f080024;
+        public static final int EdtReviewContent=0x7f08001a;
+        public static final int EdtReviewTitle=0x7f080019;
+        public static final int author_name=0x7f080020;
+        public static final int back_button=0x7f08002a;
         public static final int book_description=0x7f08000c;
         public static final int book_img=0x7f080009;
         public static final int book_summary=0x7f080012;
         public static final int book_title=0x7f08000b;
         public static final int book_toolbar=0x7f080013;
+        public static final int btnCancel=0x7f08001d;
         public static final int btnRuleScore=0x7f080015;
         public static final int btnRuleTime=0x7f080016;
+        public static final int btnSave=0x7f08001c;
         public static final int btnShowComment1=0x7f080014;
         public static final int btn_book_new_review=0x7f080017;
         public static final int content=0x7f080000;
+        public static final int linear=0x7f08001b;
         public static final int list=0x7f080004;
         public static final int list_item_layout=0x7f080008;
         public static final int loading=0x7f080005;
         public static final int mainRL=0x7f080003;
-        public static final int myTitle=0x7f080023;
+        public static final int myTitle=0x7f080029;
         public static final int progress=0x7f080006;
         public static final int ratingbar=0x7f08000a;
-        public static final int review_comments=0x7f080021;
-        public static final int review_content=0x7f080020;
-        public static final int review_summary=0x7f08001b;
-        public static final int review_title=0x7f080018;
-        public static final int review_updated=0x7f080019;
+        public static final int review_comments=0x7f080027;
+        public static final int review_content=0x7f080026;
+        public static final int review_summary=0x7f080021;
+        public static final int review_title=0x7f08001e;
+        public static final int review_updated=0x7f08001f;
         public static final int search_button=0x7f080002;
         public static final int search_text=0x7f080001;
         public static final int section1=0x7f08000e;
-        public static final int settings=0x7f08001c;
-        public static final int subject_title=0x7f08001e;
+        public static final int settings=0x7f080022;
+        public static final int subject_title=0x7f080024;
         public static final int subject_view=0x7f08000d;
-        public static final int title_bar=0x7f080022;
+        public static final int title_bar=0x7f080028;
         public static final int txtInfo=0x7f08000f;
+        public static final int txtTitle=0x7f080018;
         public static final int txt_loading=0x7f080007;
-        public static final int user_img=0x7f08001d;
-        public static final int user_info=0x7f08001f;
+        public static final int user_img=0x7f080023;
+        public static final int user_info=0x7f080025;
         public static final int wrapper=0x7f080011;
         public static final int wrapper1=0x7f080010;
     }
@@ -99,13 +109,18 @@ public final class R {
         public static final int app_name=0x7f060000;
         public static final int bookInfo=0x7f060004;
         public static final int book_search_hint=0x7f060001;
+        public static final int btnCancel=0x7f06000d;
+        public static final int btnSave=0x7f06000c;
         public static final int btnShowComment=0x7f060005;
         public static final int loading=0x7f060003;
         public static final int new_review=0x7f060008;
         public static final int order_rule=0x7f060006;
         public static final int order_rule_new=0x7f060007;
+        public static final int review_content=0x7f06000b;
+        public static final int review_info=0x7f060009;
+        public static final int review_title=0x7f06000a;
         public static final int search=0x7f060002;
-        public static final int todo=0x7f060009;
+        public static final int todo=0x7f06000e;
     }
     public static final class style {
         /** 
--- src/com/example/douban/AuthActivity.java
+++ src/com/example/douban/AuthActivity.java
@@ -1,6 +1,25 @@
 package com.example.douban;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.douban.util.ConfigData;
+import com.example.douban.util.NetUtil;
+import com.google.gdata.data.douban.UserEntry;
+import com.google.gdata.util.ServiceException;
 
 public class AuthActivity extends BaseActivity {
 	public static final int SUCCESS = 1;
@@ -10,6 +29,107 @@ public class AuthActivity extends BaseActivity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_auth);
+
+		TextView myTitle = (TextView) this.findViewById(R.id.myTitle);
+		myTitle.setText("µÇÂ¼ÊÚÈ¨");
+		((ImageButton) this.findViewById(R.id.back_button))
+				.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						AuthActivity.this.finish();
+					}
+				});
+		
+		Uri uri = this.getIntent().getData();
+		if(uri != null)
+			doAuth(uri);
+	}
+
+	void doAuth(final Uri uri) {
+		new AsyncTask<String, Void, String[]>() {
+
+			@Override
+			protected String[] doInBackground(String... args) {
+				String request_token = uri.getQueryParameter("oauth_token");
+				return saveAccessToken(request_token, NetUtil.requestTokenSecret);
+			}
+
+			@Override
+			protected void onPostExecute(String[] result) {
+				super.onPostExecute(result);
+				if (result != null) {
+					Toast toast = null;
+					toast = Toast.makeText(AuthActivity.this, "µÇÂ¼³É¹¦",
+							Toast.LENGTH_LONG);
+					toast.setGravity(Gravity.CENTER, 0, 0);
+					toast.show();
+					NetUtil.isAuthed = true;
+
+					setResult(SUCCESS);
+					finish();
+				} else {
+					doAgain();
+					NetUtil.isAuthed = false;
+				}
+			}
+
+		}.execute("");
+	}
+
+	private void doAgain() {
+		LayoutParams params = new RelativeLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		params.addRule(RelativeLayout.CENTER_IN_PARENT);
+		Button btn = new Button(getApplicationContext());
+		btn.setText("µÇÂ¼Ê§°Ü,ÇëÖØÊÔ");
+		btn.setLayoutParams(params);
+		btn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				v.setVisibility(View.GONE);
+				doAuth(false);
+			}
+		});
+		RelativeLayout layout = (RelativeLayout) AuthActivity.this
+				.findViewById(R.id.content);
+		layout.addView(btn);
+	}
+
+	String[] saveAccessToken(String requestToken, String requestTokenSecret) {
+		String[] result = null;
+
+		NetUtil.doubanService.setRequestToken(requestToken);
+		NetUtil.doubanService.setRequestTokenSecret(requestTokenSecret);
+
+		try {
+			ArrayList<String> list = NetUtil.doubanService.getAccessToken();
+			if (list == null) {
+				return null;
+			}
+			result = new String[3];
+			result[0] = list.get(0);
+			result[1] = list.get(1);
+
+			NetUtil.doubanService.setAccessToken(result[0], result[1]);
+			UserEntry user = NetUtil.doubanService.getAuthorizedUser();
+			result[2] = user.getUid();
+			System.out.println(result[2]);
+			sharedata.edit().putString(ConfigData.ACCESSTOKEN, result[0])
+					.putString(ConfigData.TOKENSECRET, result[1])
+					.putString(ConfigData.UID, result[2]).commit();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (ServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		return result;
 	}
 
 }
